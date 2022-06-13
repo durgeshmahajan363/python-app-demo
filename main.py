81df30e36ed27e2ec09e3b98013b2de94f8e9ced
durgeshmahajan <mahajandurgesh363@gmail.com>
2022-06-13 17:25:56 +0530

    Helper task completed

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from calculator import multiply, sum,subtract
+from helper import show_message
 
 print("Hello welcome to git")
 print("sum is:",sum(4,5))
 
 print("subtraction is:",subtract(5,4))
 print("multiply is:",multiply(5,4))
+
+show_message("Hello world")
