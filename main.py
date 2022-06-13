9858e28aaaf2683c65f787505a1bfec581511b54
durgeshmahajan <mahajandurgesh363@gmail.com>
2022-06-13 17:12:54 +0530

    Multiply Added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from calculator import sum
+from calculator import multiply, sum,subtract
 
 print("Hello welcome to git")
 print("sum is:",sum(4,5))
+
+print("subtraction is:",subtract(5,4))
+print("multiply is:",multiply(5,4))
