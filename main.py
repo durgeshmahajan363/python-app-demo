ccb808e295fabdada8361a505bbcf01f5749c2ac
durgeshmahajan <mahajandurgesh363@gmail.com>
2022-06-14 10:06:52 +0530

    Messanger Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from calculator import multiply, sum,subtract
 from helper import show_message
+from messanger import greet
 
 print("Hello welcome to git")
 print("sum is:",sum(4,5))
@@ -8,3 +9,4 @@ print("subtraction is:",subtract(5,4))
 print("multiply is:",multiply(5,4))
 
 show_message("Hello world")
+print(greet("Dipak"))
